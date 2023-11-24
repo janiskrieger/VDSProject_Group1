19e3917cb8a6915b31e958714352653cb7e573cd
JSeGHub <joni.singer@gmx.net>
2023-11-24 17:22:08 +0100

    added constructor test

--- src/test/Tests.h
+++ src/test/Tests.h
@@ -8,4 +8,16 @@
 #include <gtest/gtest.h>
 #include "../Manager.h"
 
+namespace ClassProject {
+    TEST(ConstructorTest, ReturnIDTrue) {
+        ClassProject::Manager m;
+        EXPECT_EQ(m.True(), 1);
+    }
+
+    TEST(ConstructorTest, ReturnIDFalse) {
+        ClassProject::Manager m;
+        EXPECT_EQ(m.False(), 0);
+    }
+}
+
 #endif

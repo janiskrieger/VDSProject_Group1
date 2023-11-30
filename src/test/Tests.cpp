57322791827609a4d2eade66250e87c339eb7ace
Janis Krieger <janis@krieger-wagner.de>
2023-11-30 10:57:20 +0100

    added constructor test

--- src/test/Tests.cpp
+++ src/test/Tests.cpp
@@ -13,4 +13,15 @@ namespace ClassProject{
         ClassProject::Manager m;
         EXPECT_EQ(m.False(), 0);
     }
+    TEST(ConstructorTest, IsConstant){
+        ClassProject::Manager m = ClassProject::Manager();
+        EXPECT_EQ(m.isConstant(0), true);
+        EXPECT_EQ(m.isConstant(1), true);
+    }
+
+    TEST(ConstructorTest, TopVarName){
+        ClassProject::Manager m = ClassProject::Manager();
+        EXPECT_EQ(m.getTopVarName(0), "false");
+        EXPECT_EQ(m.getTopVarName(1), "true");
+    }
 }

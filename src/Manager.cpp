5a5930d8af94e9dbe93c512a870770e5bbc37ef5
JSeGHub <joni.singer@gmx.net>
2023-12-07 10:41:25 +0100

    implemented remaining logic functions

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -136,6 +136,30 @@ namespace ClassProject{
         return ite(a,b,0);
     }
 
+    BDD_ID Manager::or2(BDD_ID a, BDD_ID b) {
+        return ite(a,1,b);
+    }
+
+    BDD_ID Manager::xor2(BDD_ID a, BDD_ID b) {
+        return ite(a,neg(b),b);
+    }
+
+    BDD_ID Manager::neg(BDD_ID a) {
+        return ite(a,0,1);
+    }
+
+    BDD_ID Manager::nand2(BDD_ID a, BDD_ID b) {
+        return ite(a,neg(b), 1);
+    }
+
+    BDD_ID Manager::nor2(BDD_ID a, BDD_ID b) {
+        return ite(a,0,neg(b));
+    }
+
+    BDD_ID Manager::xnor2(BDD_ID a, BDD_ID b) {
+        return ite(a, b, neg(b));
+    }
+
     BDD_ID Manager::topVar(BDD_ID f) {
         return uTable[f][VTOPVAR];
     }

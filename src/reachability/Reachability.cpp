0d82a15c3f9e0bce3b8c1d5cb2b75cc99c39d25e
Janis Krieger <janis@krieger-wagner.de>
2024-01-23 11:26:26 +0100

    implemented interface structure

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -1,1 +1,25 @@
 #include "Reachability.h"
+
+namespace ClassProject {
+    Reachability::Reachability(unsigned int stateSize, unsigned int inputSize) : ReachabilityInterface(
+            stateSize, inputSize) {
+
+    }
+
+    bool Reachability::isReachable(const std::vector<bool> &stateVector) {
+        return false;
+    }
+
+    int Reachability::stateDistance(const std::vector<bool> &stateVector) {
+        return 0;
+    }
+
+    void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {
+
+    }
+
+    void Reachability::setInitState(const std::vector<bool> &stateVector) {
+
+    }
+
+}
--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -5,9 +5,27 @@
 
 namespace ClassProject {
 
-//    class Reachability : public ReachabilityInterface {
-//
-//    };
+    class Reachability : public ReachabilityInterface {
+    private:
+
+    public:
+        Reachability(unsigned int stateSize, unsigned int inputSize);
+
+        ~Reachability() override = default;
+
+        const std::vector<BDD_ID> &getStates() const override;
+
+        const std::vector<BDD_ID> &getInputs() const override;
+
+        bool isReachable(const std::vector<bool> &stateVector) override;
+
+        int stateDistance(const std::vector<bool> &stateVector) override;
+
+        void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override;
+
+        void setInitState(const std::vector<bool> &stateVector) override;
+
+    };
 
 }
 #endif

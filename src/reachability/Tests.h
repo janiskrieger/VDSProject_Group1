7018782bec52cd851b7af447c41e62e5361aad84
Janis Krieger <janis@krieger-wagner.de>
2024-01-28 16:03:03 +0100

    extended runtime tests

--- src/reachability/Tests.h
+++ src/reachability/Tests.h
@@ -33,6 +33,39 @@ TEST_F(ReachabilityTest, HowTo_Example) { /* NOLINT */
 TEST_F(ReachabilityTest, ConstructorRuntimeErrorTest) {
     EXPECT_THROW(std::make_unique<ClassProject::Reachability>(0, 0), std::runtime_error);
     EXPECT_THROW(std::make_unique<ClassProject::Reachability>(0, 1), std::runtime_error);
+    BDD_ID s0 = stateVars2.at(0);
+    BDD_ID s1 = stateVars2.at(1);
+
+    // test for one missing transition function
+    transitionFunctions.push_back(fsm2->neg(s0));
+    EXPECT_THROW(fsm2->setTransitionFunctions(transitionFunctions), std::runtime_error);
+
+    // test invalid BDD_ID
+    transitionFunctions.push_back((BDD_ID) 999);
+    EXPECT_THROW(fsm2->setTransitionFunctions(transitionFunctions), std::runtime_error);
+    transitionFunctions.pop_back();
+
+    // test for one additional transition function
+    transitionFunctions.push_back(fsm2->neg(s1));
+    transitionFunctions.push_back(fsm2->neg(s1));
+    EXPECT_THROW(fsm2->setTransitionFunctions(transitionFunctions), std::runtime_error);
+    transitionFunctions.pop_back();
+
+    fsm2->setTransitionFunctions(transitionFunctions);
+
+    // test for one missing initial state
+    EXPECT_THROW(fsm2->setInitState({false}), std::runtime_error);
+
+    // test for one additional initial state
+    EXPECT_THROW(fsm2->setInitState({false, false, false}), std::runtime_error);
+
+    fsm2->setInitState({false, false});
+
+    // test for one missing state
+    EXPECT_THROW(fsm2->isReachable({false}), std::runtime_error);
+
+    // test for one additional state
+    EXPECT_THROW(fsm2->isReachable({false, false, false}), std::runtime_error);
 }
 
 TEST_F(ReachabilityTest, ConstructorIdentityTest) {
